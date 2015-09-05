da3cff8239458e16f6acd9eb1892f001f6d93051
eshirima <eshirima@kent.edu>
2015-09-05 05:32:09 -0400

    Storyboard changes
    Included the new stuff but the sign up view is still not completed

---
+++ MicroTrans/MicroTrans/SignUpViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SignUpViewController.swift
+//  MicroTrans
+//
+//  Created by Emil Shirima on 9/5/15.
+//  Copyright (c) 2015 Emil Shirima. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.layoutIfNeeded()
+//        scrollView.contentSize = 
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
